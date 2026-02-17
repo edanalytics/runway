60d5c89edaf89e1e58ed0f058085c0da6013a6df
johncmerfeld <John.Merfeld@gmail.com>
2026-02-17 13:24:00 -0600

    note changes

--- executor/executor/earthmover_hints.py
+++ executor/executor/earthmover_hints.py
@@ -1,11 +1,14 @@
-
+# These messages may be found in Earthmover error logs. Given our already tight coupling to Earthmover, 
+# it seems reasonable to use these as hints 
 
 CODEC = "codec can't decode"
+
 COLUMNS_MISSING = "One or more columns specified are not present in the dataset"
+INPUT_VALIDATION = "Source `$sources.input` failed expectation"
+
 DATE_FORMAT = "ValueError: time data"
 EXCEL = "loading an Excel source requires additional"
-INPUT_VALIDATION = "Source `$sources.input` failed expectation"
 TXT_FILE = "No `colspec_file` specified for fixedwidth source"
 
-REGEX_COLUMNS = "specified .* `columns` but has .* columns" # lol, require this be a number?
-REGEX_COLSPEC_FILE = "colspec file .* not found"
+REGEX_COLUMNS = r"specified .* `columns` but has .* columns" # expecting that this be a number seems like overkill
+REGEX_COLSPEC_FILE = r"colspec file .* not found"
