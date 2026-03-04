3d752d7a53928789378238c00f1c6e52fafef84b
johncmerfeld <John.Merfeld@gmail.com>
2026-03-04 14:57:33 -0600

    init

---
+++ executor/executor/id_mapper.py
@@ -0,0 +1,26 @@
+class IdMapper():
+    """Responsible for proposing a mapping between student IDs that do not exist in the roster to IDs that do"""
+
+    def __init__(self):
+        # unmatched_students file
+        # enrollment file
+        # roster file
+        # ID to use in enrollments
+        # ID column in assessment
+        # fuzzy matching columns mapping in assessment
+        pass
+
+    def build_vector_db(self):
+        pass
+
+    def query_vector_db(self):
+        pass
+
+    def map_record(self):
+        pass
+
+    def produce_mapping(self):
+        pass
+
+
+    
