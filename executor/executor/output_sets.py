724bd6a1416ecbb8dc5e2cc475978e20582abcb6
johncmerfeld <John.Merfeld@gmail.com>
2026-06-05 11:43:30 -0500

    clean up how the summary files filter works

--- executor/executor/output_sets.py
+++ executor/executor/output_sets.py
@@ -4,9 +4,7 @@
 # two output sets.
 
 import json
-
-
-NON_RESOURCE_DESTINATIONS = {"student_id_match_rates", "input_no_student_id_match"}
+import os
 
 
 class OutputSet:
@@ -37,9 +35,14 @@ class OutputSet:
         with open(self.em_results_path) as f:
             em_results = json.load(f)
         dest_prefix = "$destinations."
-        return {
-            key[len(dest_prefix):]: {"records_processed": count}
-            for key, count in em_results.get("row_counts", {}).items()
-            if key.startswith(dest_prefix)
-            and key[len(dest_prefix):] not in NON_RESOURCE_DESTINATIONS
-        }
+        resources = {}
+        # Earthmover produces a bunch of files; the summary must only contain
+        # information about the data outputs, i.e. JSONL
+        for key, count in em_results.get("row_counts", {}).items():
+            if not key.startswith(dest_prefix):
+                continue
+            name = key[len(dest_prefix):]
+            if not os.path.exists(os.path.join(self.local_dir, name + ".jsonl")):
+                continue
+            resources[name] = {"records_processed": count}
+        return resources
