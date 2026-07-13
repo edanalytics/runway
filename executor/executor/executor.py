1d20cd8b1bc4381139ce128a447559b122c9f983
Theo Kaufman <107571812+theokaufman@users.noreply.github.com>
2026-07-13 09:29:13 -0400

    Fix/return code (#96)
    * remove check_returncode() from earthmover_cmd
    
    * add back explicit calls of check_returncode()

--- executor/executor/executor.py
+++ executor/executor/executor.py
@@ -266,9 +266,6 @@ class JobExecutor:
         if em.stderr:
             self.logger.info(f"earthmover stderr: {em.stderr}")
 
-        # Raise CalledProcessError if return code is non-zero
-        em.check_returncode()
-
         return em       
 
     def earthmover_deps(self):
@@ -489,11 +486,13 @@ class JobExecutor:
         try:
             cmd = ["earthmover", "-c", self.wrapper_earthmover, "compile"]
             em = self.earthmover_cmd(args=cmd, capture_output=True, text=True)
+            em.check_returncode()
 
             # attempt no. 1
             cmd = ["earthmover", "-c", self.wrapper_earthmover, "run", "--results-file", results_path]
             cmd.extend(encoding_args)
             em = self.earthmover_cmd(args=cmd, capture_output=True, text=True)
+            em.check_returncode()
 
         except subprocess.CalledProcessError as err:
             self.logger.error("earthmover encountered an error")
@@ -507,7 +506,8 @@ class JobExecutor:
                     # attempt no. 2 - need a new em object to overwrite the decoding error
                     cmd = ["earthmover", "-c", self.wrapper_earthmover, "run", "--results-file", results_path, "--set", "sources.input.encoding", "iso-8859-1"]
                     em = self.earthmover_cmd(args=cmd, capture_output=True, text=True)
-
+                    em.check_returncode()
+                    
                     fatal = False # if we made it this far, we can abort the shutdown
                 except subprocess.CalledProcessError:
                     # failed again, move on to shutdown procedure
